02b4f5ae3760d31b6f5bf50557b5a5f9cbfdb70f
patrick-zippenfenig <patrick@zippenfenig.de>
2023-09-29 16:23:28 +0200

    testing

--- index.ts
+++ index.ts
@@ -3,4 +3,23 @@
 require = require("esm")(module/* , options */)
 module.exports = require("./main.ts")
 
-console.log("test")
+import { WeatherApi } from './com/openmeteo/weather-api'
+
+
+async function test() {
+    var params =  {
+        latitude: [52.52, 48.1, 48.4],
+        longitude: [13.41, 9.31, 8.5],
+        hourly: ['temperature_2m', 'precipitation'],
+        timezone: 'auto',
+        current: ['temperature_2m','precipitation'],
+        format: 'flatbuffers'
+    }
+    var response = await fetch("https://api.open-meteo.com/v1/forecast?"+ new URLSearchParams(params))
+    console.log(response)
+
+    let result = WeatherApi.getSizePrefixedRootAsWeatherApi(response.arrayBuffer)
+    console.log(result.latitude)
+}
+
+test()
