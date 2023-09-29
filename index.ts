c0c33fadfd8a11a4221e8072e622b79947971391
patrick-zippenfenig <patrick@zippenfenig.de>
2023-09-29 16:15:56 +0200

    print

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Set options as a parameter, environment variable, or rc file.
+// eslint-disable-next-line no-global-assign
+require = require("esm")(module/* , options */)
+module.exports = require("./main.ts")
+
+console.log("test")
