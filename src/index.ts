b08b165c2557e5a15a92d67e42fd0e041ecb1264
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-19 19:59:23 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,29 @@
 import { ByteBuffer } from 'flatbuffers';
 import { WeatherApiResponse } from '@openmeteo/sdk/weather-api-response';
 
-async function test_api_fetch() {
-  const params = {
-    latitude: '52.52,48.1,48.4',
-    longitude: '13.41,9.31,8.5',
-    hourly: 'temperature_2m,precipitation,windspeed_10m',
-    timezone: 'auto',
-    current: 'temperature_2m,precipitation',
-    format: 'flatbuffers',
-  };
-  const urlParams = new URLSearchParams(params).toString();
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?${urlParams}`
-  );
+async function weather_api(
+  url: string,
+  params: any
+): Promise<WeatherApiResponse[]> {
+  const urlParams = new URLSearchParams(params);
+  urlParams.set('format', 'flatbuffers');
+  const response = await fetch(`${url}?${urlParams.toString()}`);
   const bb = await response.arrayBuffer();
   const fb = new ByteBuffer(new Uint8Array(bb));
+
+  // TODO: retry, error handling
 
   const results: WeatherApiResponse[] = [];
   let pos = 0;
   while (pos < fb.capacity()) {
+    fb.setPosition(pos);
     const len = fb.readInt32(fb.position());
     results.push(
       WeatherApiResponse.getSizePrefixedRootAsWeatherApiResponse(fb)
     );
     pos += len + 4;
-    fb.setPosition(pos);
   }
   return results;
 }
 
-export { test_api_fetch, WeatherApiResponse };
-// const myPackage = (taco = ''): string => `${taco} from my package`;
+export { weather_api };
