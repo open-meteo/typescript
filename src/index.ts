6cd77a641baee287d8288802cc631ebda32cdbbb
patrick-zippenfenig <patrick@zippenfenig.de>
2023-10-30 15:37:01 +0100

    update test

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 import { ByteBuffer } from 'flatbuffers';
-import { WeatherApiResponse } from '@openmeteo/sdk/weather-api-response';
+import { ApiResponse } from '@openmeteo/sdk/api-response';
 
 async function weather_api(
   url: string,
   params: any
-): Promise<WeatherApiResponse[]> {
+): Promise<ApiResponse[]> {
   const urlParams = new URLSearchParams(params);
   urlParams.set('format', 'flatbuffers');
+  console.log(`${url}?${urlParams.toString()}`);
   const response = await fetch(`${url}?${urlParams.toString()}`);
   const bb = await response.arrayBuffer();
   const fb = new ByteBuffer(new Uint8Array(bb));
 
   // TODO: retry, error handling
 
-  const results: WeatherApiResponse[] = [];
+  const results: ApiResponse[] = [];
   let pos = 0;
   while (pos < fb.capacity()) {
     fb.setPosition(pos);
     const len = fb.readInt32(fb.position());
     results.push(
-      WeatherApiResponse.getSizePrefixedRootAsWeatherApiResponse(fb)
+      ApiResponse.getSizePrefixedRootAsApiResponse(fb)
     );
     pos += len + 4;
   }
